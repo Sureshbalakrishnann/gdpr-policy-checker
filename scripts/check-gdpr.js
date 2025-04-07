416c76e33254ef9b8ec5f1efbf4eac6fed57de11
suresh-balakri <Suresh_Balakrishnan@condenast.com>
2025-04-07 18:19:33 +0530

    policy content1111

--- scripts/check-gdpr.js
+++ scripts/check-gdpr.js
@@ -19,7 +19,7 @@ async function cloneOrUpdateRepo() {
     const repo = simpleGit(LOCAL_REPO_PATH);
 
     await repo.checkout("gdpr-fix-branch");
-    await repo.pull("origin", "magdpr-fix-branchin");
+    await repo.pull("origin", "gdpr-fix-branchin");
   } else {
     console.log("📦 Cloning repo...");
     await git.clone(GITHUB_REPO_URL, LOCAL_REPO_PATH);
